a21c331379025dc898afc61254a66ab28e7501bd
ashneet kaur tutej <tutejaashneet@gmail.com>
2025-04-14 00:36:04 +0530

    taking input from cmd addition of two

--- 09-functions.sh
+++ 09-functions.sh
@@ -3,15 +3,18 @@ USERID=$(id -u)
 DATE=$(date +%F)
 SCRIPTNAME=$0
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 #this function should validate previous commad 
 VALIDATE(){
  if [ $1 -ne 0 ]
 then
-echo "$2is  failed"
+echo "$2is  $R failed $N"
 exit 1
 else 
-echo "$2 is succesful"
+echo "$2 is $G succesful $N"
 fi
 }
 

945cf14004fc49c00c3e359d8bb120b7a90188e0
ashneet kaur tutej <tutejaashneet@gmail.com>
2025-04-13 20:53:15 +0530

    hello worls script added

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Hello world, I am lerning Devops with Aws"
+
+echo "Ramesh:Hi suresh ,GOOd morning"
+echo "suresh:Good morning ramesh"
+echo "Ramesh:Hi suresh , i am good how are you"

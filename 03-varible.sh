ee0afa1340a878e6b403fe3ed1d29875a00ebdf9
ashneet kaur tutej <tutejaashneet@gmail.com>
2025-04-13 22:41:02 +0530

    taking input from cmd

---
+++ 03-varible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello world, I am learning Devops with Aws"
+PERSON1=$1 # this is the first value from cmd
+PERSON2=$2 # this is the second value from cmd
+
+echo "$PERSON1:Hi $PERSON2 ,GOOd morning"
+echo "$PERSON2:Good morning $PERSON1"
+echo "$PERSON1:Hi $PERSON2 , i am good how are you"

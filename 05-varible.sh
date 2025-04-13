f5976bf8fe0516094737594ca820057b3480cdfd
ashneet kaur tutej <tutejaashneet@gmail.com>
2025-04-13 22:52:42 +0530

    taking input from cmd addition of two

--- 05-varible.sh
+++ 05-varible.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Please enter your Username"
 read USERNAME # this is the variabble where the value of the user is enterd
-echo "USERNAME entered is $USERNAME"
+echo "USERNAME entered is $USERNAME
+
+echo "Please enter your Password"
+ read PASSWORD 
+
+ echo "password enterd is $PASSWORD"
